e8883802abfcb371d616b0299e6aaf87782b518f
Wilker <wilkerbranquinho@gmail.com>
2024-11-02 11:54:06 -0300

    Add arquivos ao projeto

--- MauiAppLogin/App.xaml.cs
+++ MauiAppLogin/App.xaml.cs
@@ -8,5 +8,14 @@
 
             MainPage = new AppShell();
         }
-    }
-}
+        protected override Window CreateWindow(IActivationState ActivationState) //Permite modificar o tamanho da janela
+        {
+            var window = base.CreateWindow(ActivationState);
+            window.Width = 400; //Largura de Janela
+            window.Height = 700; //Altura de Janela
+            return window;
+        }
+    } //Fecha Classe
+
+
+} //Fecha NameSpace 
